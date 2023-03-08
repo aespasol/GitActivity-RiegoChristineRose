44bc84c47ff0334c5f6501d83ce9216ce31a9c47
RiegoChristineRose <christineroseriego0125@gmail.com>
2023-03-08 16:16:48 +0800

    My Educational Background added

--- git-python-riego.py
+++ git-python-riego.py
@@ -6,3 +6,7 @@
 print("Christine Rose Riego")
 print("Polo, Santa Cruz, Marinduque")
 print("September 01, 2003")
+print("Marinduque State College")
+print("Bachelor of Science in Information Technology")
+print("2nd year")
+print("Section C")
